198c8d8240e595c2bf5e5cf42e153498a9e158b5
linxin <julycoolwind@hotmail.com>
2014-01-02 16:34:21 +0800

    增加DZPhone的单元测试类

---
+++ 定制短信 Tests/DZPhoneTest.m
@@ -0,0 +1,34 @@
+//
+//  DZPhoneTest.m
+//  DZSMSSender
+//
+//  Created by linx on 14-1-2.
+//  Copyright (c) 2014年 linx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DZPhoneTest : XCTestCase
+
+@end
+
+@implementation DZPhoneTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
